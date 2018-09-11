02d6a21a49c8779fcfa9ce5bc5b5063698a75892
rolyne chen <rolynepp@gmail.com>
2018-09-11 17:08:51 +0800

    test frequency

--- test.py
+++ test.py
@@ -1,12 +1,15 @@
 import random
 r = random.randint(1 ,100)
+x = 1
 while True:
 	a = input('請輸入數字: ')
 	a = int(a)
 	if a == r:
 		print('終於猜對了')
+		break
 	elif a > r :
 		print('比答案大')
 	else:
 		print('比答案小')
-
+	print('你總共輸入了',x ,'次')
+	x = x + 1
