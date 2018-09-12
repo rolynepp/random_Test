526b966b474f25c0149364c1470932871b79db95
rolyne chen <rolynepp@gmail.com>
2018-09-12 17:15:07 +0800

    set range

--- test.py
+++ test.py
@@ -1,5 +1,10 @@
 import random
-r = random.randint(1 ,100)
+s = input('請決定隨機數字,開始: ')
+e = input('請決定隨機數字,結尾: ')
+s = int(s)
+e = int(e)
+
+r = random.randint(s ,e)
 x = 1
 while True:
 	a = input('請輸入數字: ')
@@ -12,4 +17,5 @@ while True:
 	else:
 		print('比答案小')
 	print('你總共輸入了',x ,'次')
-	x = x + 1
+	x = x + 1    #新更改的地方是為了顯示出key進去的次數
+	# x += 1 及是x = x + 1的快寫法
